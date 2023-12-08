f1790821aae1d6d30414da5f446dcd39ee162d21
Matt Nappo <mattnappo@gmail.com>
2023-12-07 22:31:44 -0500

    One unified UI

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
+use serde_json::json;
+use std::error::Error;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
-
-const TEST_SELECT1: &str = r#"{"jsonrpc":"2.0","id":"id","method":"select","params":{"db_name":"people", "table_name": "friends"}}"#;
-
-#[derive(Debug, Default)]
-struct Model {
-    x: i64,
-    y: i64,
-}
-
-impl Model {
-    pub fn new(x: i64, y: i64) -> Self {
-        Self { x, y }
-    }
-}
 
 mod protocol {
     use serde::{Deserialize, Serialize};
@@ -76,46 +64,25 @@ mod resource {
     }
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let state = use_state(|| Model::default());
+/* +------------------------------------+ */
+/* |             MAIN CODE              | */
+/* +------------------------------------+ */
 
-    let onclick_x = {
-        let state = state.clone();
-        move |_| state.set(Model::new(state.x + 1, state.y))
-    };
-    let onclick_y = {
-        let state = state.clone();
-        move |_| state.set(Model::new(state.x, state.y + 1))
-    };
-
-    //let select = |_| resource::select();
-
-    html!(
-        <div>
-            <button onclick={onclick_x}>{"add one to x"}</button>
-            <button onclick={onclick_y}>{"add one to y"}</button>
-            //<button onclick={select}>{"select"}</button>
-            <p>{format!("state: {:?}", state)}</p>
-            <TextInput name="query" />
-        </div>
-    )
-}
-
+/*
 #[derive(Properties, PartialEq)]
 pub struct Props {
     name: String,
 }
 
 #[derive(Default)]
-struct State {
+struct QueryState {
     query: String,
-    results: String,
 }
 
-#[function_component(TextInput)]
-fn text_input(props: &Props) -> Html {
-    let state = use_state(State::default);
+/// A textbox and a button
+#[function_component(Query)]
+fn query(props: &Props) -> Html {
+    let state = use_state(QueryState::default);
 
     let onchange = {
         let state = state.clone();
@@ -123,40 +90,22 @@ fn text_input(props: &Props) -> Html {
             let target = event.target().unwrap();
             //println!("{:?}", target);
             let input = target.unchecked_into::<HtmlInputElement>();
-            state.set(State {
+            state.set(QueryState {
                 query: input.value(),
-                results: state.results.clone(),
             });
         }
     };
 
-    let select = {
-        let state = state.clone();
-        // Make request
-        wasm_bindgen_futures::spawn_local(async move {
-            let res = resource::select(&state.query).await.unwrap();
-            state.set(State {
-                query: state.query.clone(),
-                results: res,
-            })
-        })
-
-        // Update state
-    };
-
-    /*
     html! {
         <div>
             <input type="text" name={props.name.clone()} onchange={onchange} />
-            <button onclick={select}>{"select"}</button>
-            <p>{state.results.clone()}</p>
+            // <button onclick={select}>{"select"}</button>
         </div>
     }
-    */
-
-    html!()
 }
+*/
 
+#[derive(Clone)]
 enum ComponentLoadingStage {
     Loading,
     Success,
@@ -169,38 +118,165 @@ impl Default for ComponentLoadingStage {
     }
 }
 
+//#[derive(Default)]
+//struct Dataview {
+//    data: String,
+//    loading_stage: ComponentLoadingStage,
+//}
+
 #[derive(Default)]
-struct Query {
-    results: String,
+struct State {
+    query: String,
     loading_stage: ComponentLoadingStage,
 }
 
-impl Component for Query {
+#[derive(Properties, PartialEq)]
+struct Props;
+
+#[derive(PartialEq, Clone)]
+struct Message {
+    data: String,
+}
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct SelectRes {
+    jsonrpc: String,
+    id: String,
+    method: String,
+    params: Vec<serde_json::Value>,
+}
+
+fn pretty(s: &str) -> String {
+    let data = serde_json::from_str::<SelectRes>(s).unwrap();
+    let json = json!( { "response": data } );
+    format!("{json}")
+}
+
+use std::rc::Rc;
+
+impl Reducible for Message {
+    type Action = String;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        Message { data: action }.into()
+    }
+}
+
+pub type MessageContext = UseReducerHandle<Message>;
+
+#[derive(Properties, Debug, PartialEq)]
+pub struct MessageProviderProps {
+    #[prop_or_default]
+    pub children: Html,
+}
+
+#[function_component]
+pub fn MessageProvider(props: &MessageProviderProps) -> Html {
+    let msg = use_reducer(|| Message {
+        data: "No data yet.".to_string(),
+    });
+
+    html! {
+        <ContextProvider<MessageContext> context={msg}>
+            {props.children.clone()}
+        </ContextProvider<MessageContext>>
+    }
+}
+
+#[function_component]
+fn Dataview(_props: &Props) -> Html {
+    let state = use_state(State::default);
+    let ctx = use_context::<MessageContext>().unwrap();
+
+    let textbox_onchange = {
+        let state = state.clone();
+        move |event: Event| {
+            let target = event.target().unwrap();
+            let input = target.unchecked_into::<HtmlInputElement>();
+            state.set(State {
+                query: input.value(),
+                loading_stage: state.loading_stage.clone(),
+            });
+        }
+    };
+
+    let ctx2 = ctx.clone();
+    let onclick = {
+        let state = state.clone();
+        move |_| {
+            // Fetch the data
+            let q = state.query.clone();
+
+            let msg_ctx1 = ctx.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let result = resource::select(&q.to_string()).await;
+                msg_ctx1.dispatch(result.unwrap());
+            });
+
+            //let message = msg_ctx.data.to_owned();
+
+            // Update the state
+            state.set(State {
+                query: state.query.clone(),
+                loading_stage: state.loading_stage.clone(),
+            });
+        }
+    };
+
+    html! {
+        <div style="width: 800px;">
+            <textarea rows=20 cols=80 type="text" name="query" onchange={textbox_onchange} />
+            <br />
+            <button {onclick}>{"execute query"}</button>
+            <hr />
+            <code>
+                {ctx2.data.to_owned().clone()}
+            </code>
+        </div>
+    }
+}
+
+/*
+struct OldDataview;
+impl Component for OldDataview {
     type Message = Result<String, Box<dyn std::error::Error>>;
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
-        Query::default()
+    fn create(_ctx: &Context<Self>) -> Self {
+        OldDataview
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        match self.loading_stage {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let onchange = {
+            let
+            move |event: Event| {
+                let target = event.target().unwrap();
+                //println!("{:?}", target);
+                let input = target.unchecked_into::<HtmlInputElement>();
+                state.set(QueryState {
+                    query: input.value(),
+                });
+            }
+        };
+
+        let data = match self.loading_stage {
             ComponentLoadingStage::Loading => {
                 html! { <h1>{"querying..."}</h1>}
             }
-            /*
-            ComponentLoadingStage::Success => self
-                .lesson_plans
-                .iter()
-                .map(|lp| {
-                    html! { <ViewLessonPlan lesson_plan={lp.clone()} /> }
-                })
-                .collect::<Html>(),
-            */
-            ComponentLoadingStage::Success => html! { <p>{self.results.clone()}</p> },
+            ComponentLoadingStage::Success => html! { <p>{self.data.clone()}</p> },
             ComponentLoadingStage::Error => {
                 html! { <h1>{ "error" }</h1>}
             }
+        };
+
+        html! {
+            <div>
+                <input type="text" name="query" onchange={onchange} />
+                //<button onclick={select}>{"select"}</button>
+                {data}
+            </div>
         }
     }
 
@@ -208,16 +284,16 @@ impl Component for Query {
         if first_render {
             let link = ctx.link().clone();
             wasm_bindgen_futures::spawn_local(async move {
-                let result = resource::select(TEST_SELECT1).await;
+                let result = resource::select(&self.query).await;
                 link.send_message(result);
             });
         }
     }
 
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Ok(results) => {
-                self.results = results;
+            Ok(data) => {
+                self.data = data;
                 self.loading_stage = ComponentLoadingStage::Success;
             }
             Err(_) => {
@@ -228,7 +304,20 @@ impl Component for Query {
     }
 }
 
+*/
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <MessageProvider>
+            <div>
+                <h1>{"ToyDB Web Client"}</h1>
+                <Dataview />
+            </div>
+        </MessageProvider>
+    }
+}
+
 fn main() {
-    //yew::Renderer::<App>::new().render();
-    yew::Renderer::<Query>::new().render();
+    yew::Renderer::<App>::new().render();
 }
